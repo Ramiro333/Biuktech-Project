9983e876c482e3bcdcaf84dc3e671d92f94be254
Ramiro333 <ramirololreyna@gmail.com>
2025-10-06 22:06:52 -0300

    mejorado fondo

---
+++ assets/js/particle.js
@@ -0,0 +1,92 @@
+    document.addEventListener("DOMContentLoaded", () => {
+    tsParticles.load("background", { 
+        fpsLimit: 60,
+        background: {
+            color: "transparent",
+        },
+        particles: {
+            number: {
+                value: 50,
+                density: { enable: true, value_area: 800 },
+            },
+            color: { value: ["#83009D", "#00BD9D"] },
+            shape: { type: "circle" },
+            opacity: {
+                value: 0.6,
+                random: true,
+            },
+            size: {
+                value: 3,
+                random: true,
+            },
+            move: {
+                enable: true,
+                speed: 1.2,
+                direction: "none",
+                outModes: { default: "out" },
+            },
+            links: {
+                enable: true,
+                distance: 150,
+                color: "#83009D",
+                opacity: 0.3,
+                width: 1,
+            },
+        },
+        interactivity: {
+            events: {
+                onHover: { enable: true, mode: "repulse" },
+                resize: true,
+            },
+            modes: {
+                repulse: { distance: 100, duration: 0.4 },
+            },
+        },
+        detectRetina: true,
+    });
+});
+    // tsParticles.load("background", {
+    //     fpsLimit: 60,
+    //     background: {
+    //         color: "transparent",
+    //     },
+    //     particles: {
+    //         number: {
+    //             value: 50,
+    //             density: { enable: true, value_area: 800 },
+    //         },
+    //         color: { value: ["#83009D", "#00BD9D"] },
+    //         shape: { type: "circle" },
+    //         opacity: {
+    //             value: 0.6,
+    //             random: true,
+    //         },
+    //         size: {
+    //             value: 3,
+    //             random: true,
+    //         },
+    //         move: {
+    //             enable: true,
+    //             speed: 1.2,
+    //             direction: "none",
+    //             outModes: { default: "out" },
+    //         },
+    //         links: {
+    //             enable: true,
+    //             distance: 150,
+    //             color: "#83009D",
+    //             opacity: 0.3,
+    //             width: 1,
+    //         },
+    //     },
+    //     interactivity: {
+    //         events: {
+    //             onHover: { enable: true, mode: "repulse" },
+    //             resize: true,
+    //         },
+    //         modes: {
+    //             repulse: { distance: 100, duration: 0.4 },
+    //         },
+    //     },
+    //     detectRetina: true,
+    // });
